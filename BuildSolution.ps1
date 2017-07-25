24ed347b4e6ce0ed63c0bad4454b5239c59823b5
Eoin O'Connor <eoin55@gmail.com>
2017-07-25 23:18:57 +1000

    [NoTicket] - Downgraded the DotNet Core version so that it's compatible with VS 2015.

--- BuildSolution.ps1
+++ BuildSolution.ps1
@@ -1,10 +1,9 @@
 param([string] $buildType = "Release")
 
 pushd .\Src\Jobbie.Sample.UI.Host
-msbuild -t:restore
 npm install
 webpack
 popd
 
 nuget restore .\Src\Jobbie.sln
-msbuild -t:build .\Src\Jobbie.sln -p:configuration=$buildType
+dotnet build .\Src\Jobbie.sln --configuration=$buildType
